ae89441599089f405f5a2408f86a6b8c81f8c3c4
Repos-For-Deploy <sourav.roy90001@gmail.com>
2025-07-31 18:17:16 -0500

    update

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+import os
+
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
+MYSQL_DB = os.getenv("MYSQL_DB", "email_checker")
